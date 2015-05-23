f5c31589cbd3e25d579d92d59d8b23aea946e150
runningtalus <dave@davemay.org>
2015-05-23 11:57:03 -0600

    Fluid layouts via percentages.

--- main.js
+++
@@ -1,8 +0,0 @@
-$('#menu').click(function(){
-  $('nav').toggleClass('is-expanded');
-});
-
-$('.has-subnav a').click(function(){
-  $(this).parent().toggleClass('is-expanded');
-  $('nav').toggleClass('is-subnav-expanded');
-});
