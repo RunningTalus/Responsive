006e8284dc805262929b55bd994464bbd72d8a8d
runningtalus <dave@davemay.org>
2015-05-30 15:01:35 -0600

    Using JS and Modernizr for conditional loading of svg and img assets.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+var path = $("#star-svg").attr("data-path")
+var svgObj;
+
+if (Modernizr.svg) {
+  console.log(path)
+  svgObj = "<object type=\"image/svg+xml\"  style=\"float:right\" data=\""+ path +".svg\"></object>"
+} else {
+  svgObj = "<img src=\""+ path +".png\">"
+}
+
+$("#star-svg").html(svgObj);
+
 $("#infoTray").click(function(){
   $("body").toggleClass("info-active");
 });
